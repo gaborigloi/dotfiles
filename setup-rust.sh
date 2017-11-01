38c0ad9cdfcda8cda821d9b3eecda9ec1edc8c37
Gabor Igloi <gabor.igloi@citrix.com>
2017-11-01 09:46:13 +0000

    setup-rust.sh: add flag to skip GUI apps
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- setup-rust.sh
+++ setup-rust.sh
@@ -10,17 +10,19 @@ cargo install ripgrep
 cargo install --git https://github.com/sharkdp/fd
 cargo install ion-shell
 
-# from https://github.com/jwilm/alacritty
-cd ~/opt-src
-git clone https://github.com/jwilm/alacritty.git
-cd alacritty
-sudo apt-get install cmake libfreetype6-dev libfontconfig1-dev xclip
-cargo build --release
-cp target/release/alacritty ~/.local/bin/
+if [ -z $NOX ]; then
+  # from https://github.com/jwilm/alacritty
+  cd ~/opt-src
+  git clone https://github.com/jwilm/alacritty.git
+  cd alacritty
+  sudo apt-get install cmake libfreetype6-dev libfontconfig1-dev xclip
+  cargo build --release
+  cp target/release/alacritty ~/.local/bin/
 
-# from https://github.com/greshake/i3status-rust
-cd ~/opt-src
-git clone https://github.com/XYunknown/i3status-rust.git
-sudo apt install fonts-powerline
-sudo apt install fonts-font-awesome
-cd i3status-rust && cargo build --release
+  # from https://github.com/greshake/i3status-rust
+  cd ~/opt-src
+  git clone https://github.com/XYunknown/i3status-rust.git
+  sudo apt install fonts-powerline
+  sudo apt install fonts-font-awesome
+  cd i3status-rust && cargo build --release
+fi
