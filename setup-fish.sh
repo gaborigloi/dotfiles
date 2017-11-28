0fbae33b854b6d3621b732aa0492fbaa04920cce
Gabor Igloi <gabor.igloi@citrix.com>
2017-11-28 09:50:06 +0000

    Move fish config files into separate directory
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- setup-fish.sh
+++ setup-fish.sh
@@ -4,9 +4,9 @@ set -eux
 
 # https://wiki.archlinux.org/index.php/Fish
 mkdir -p ~/.config/fish/functions/
-ln -s $PWD/config.fish ~/.config/fish/
-ln -s $PWD/fish_prompt.fish ~/.config/fish/functions/
+ln -s $PWD/fish/config.fish ~/.config/fish/
+ln -s $PWD/fish/fish_prompt.fish ~/.config/fish/functions/
 
 echo 'exec fish' >> ~/.bashrc
 fish -c fish_update_completions
-fish abbr.fish
+fish fish/abbr.fish
