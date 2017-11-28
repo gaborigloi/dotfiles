afd5743bbbb602be7cd91f0f335333915861a8be
Gabor Igloi <gabor.igloi@citrix.com>
2017-11-28 09:43:43 +0000

    setup-opam.sh: install newer ocaml, install packages in one step
    To avoid unnecessary recompilations.
    
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- setup-opam.sh
+++ setup-opam.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
 # initialise opam
-opam init --compiler=4.02.3 --auto-setup --yes
+opam init --compiler=4.04.2 --auto-setup --yes
 eval `opam config env`
 echo OPAM setup has to be in .profile
 
 opam remote add xs-opam git://github.com/xapi-project/xs-opam.git
 opam install --yes depext
-opam depext --yes xapi
-opam install --yes xapi
-
-opam install --yes utop
-opam install --yes merlin
-opam install --yes ocp-indent
-opam install --yes ocp-browser
+opam depext --yes xapi utop merlin ocp-indent ocp-browser
+opam install --yes xapi utop merlin ocp-indent ocp-browser
 
