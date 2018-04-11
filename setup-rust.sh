1f7437fdda8c703f521d7ac92dc9bd8f81d6e11f
Gabor Igloi <gabor.igloi@citrix.com>
2018-04-11 10:03:33 +0100

    Setup rustup completions
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- setup-rust.sh
+++ setup-rust.sh
@@ -4,6 +4,8 @@ set -uex
 
 curl https://sh.rustup.rs -sSf | sh
 source "$HOME/.cargo/env"
+mkdir -p ~/.config/fish/completions
+rustup completions fish > ~/.config/fish/completions/rustup.fish
 
 cargo install cargo-update
 sudo dnf install ripgrep || cargo install ripgrep
